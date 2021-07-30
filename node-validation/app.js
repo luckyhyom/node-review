ef6d092afbb9f2f30b6e3f58169f4736419b4407
luckyhyom <bs_khm@naver.com>
2021-07-31 00:09:43 +0900

    validator 사용

--- node-validation/app.js
+++ node-validation/app.js
@@ -1,20 +1,35 @@
 import express from 'express';
-import {body} from 'express-validator';
+import {body, validationResult,param} from 'express-validator';
 
 const app = express();
 
 app.use(express.json());
 
+function validation(req,res,next) { 
+    const error = validationResult(req);
+    if(error.isEmpty()){
+        console.log('test');
+        return next();
+    }
+
+    return res.status(400).send({"message":error.array()});
+}
+
 app.post(
     '/id',
-    body('name').isLength({ min: 3 }).withMessage('it`s way too short!'),
+    [body('name').isLength({ min: 3 }).withMessage('it`s way too short!'),validation],
     (req,res)=> {
         console.log(req.body);
         res.send(req.body);
 });
 
-app.post('/:message',(req,res)=>{
-
+app.get('/:email',
+    [param('email').isEmail().withMessage('this is supposed to be Email'),
+    validation],
+(req,res)=>{
+console.log(req.params);
+// message라는 이름으로 값을 담는다.
+res.send(req.body);
 });
 
 app.use((err,req,res,next)=>{
